f9ed88d75c4b8e3fd011e19d958af649c33eecec
Ali Bahadoran <alibahadoran.eng@gmail.com>
2022-07-16 15:38:04 +0430

    End Project

---
+++ js/script.js
@@ -0,0 +1,17 @@
+const selectAll = document.getElementById("SelectAll");
+const select = document.querySelectorAll("#select");
+const btn = document.querySelector("button");
+
+selectAll.onclick = () => {
+    if (selectAll.checked == true) {
+        for (i = 0; i < select.length; i++) {
+            select[i].checked = true;
+            btn.classList.add("active");
+        }
+    } else {
+        for (i = 0; i < select.length; i++) {
+            select[i].checked = false;
+            btn.classList.remove("active");
+        }
+    }
+};
